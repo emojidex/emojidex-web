cbfb7cf8862c7503e8591b9ef7f87b96d3f91df8
chikusa <yusuke1207a@gmail.com>
2013-10-09 00:03:38 +0900

    wip

---
+++ src/assets/js/jquery-emojiarea/packs/basic/emojis.js
@@ -0,0 +1,8 @@
+$.emojiarea.path = 'assets/js/jquery-emojiarea/packs/basic';
+$.emojiarea.icons = {
+	':smile:'     : 'smile.png',
+	':angry:'     : 'angry.png',
+	':flushed:'   : 'flushed.png',
+	':neckbeard:' : 'neckbeard.png',
+	':laughing:'  : 'laughing.png'
+};
