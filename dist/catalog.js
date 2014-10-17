5fbbd0376bcf87086338f2ae1d5a4f30c1e3261b
giurasu <giurasu1192.gmail.com>
2014-10-17 12:28:12 +0900

    wip: sass: start

--- dist/catalog.js
+++ dist/catalog.js
@@ -1,33 +1,31 @@
 (function() {
-  var name, signal, x;
-
-  console.log(222);
-
-  name = "aaa";
-
-  console.log(name);
-
-  signal = "blue";
-
-  switch (signal) {
-    case "red":
-      alert("stop");
-      break;
-    case "blue":
-      alert("go");
-      break;
-    default:
-      console.log("owari");
-  }
-
-  x = 50;
-
-  if (x < 30) {
-    console.log("OK");
-  } else if (x < 40) {
-    alert("NO");
-  } else {
-    alert("sankusu");
-  }
+  (function() {
+    $(document).ready(function() {
+      return $.ajax({
+        url: "https://www.emojidex.com/api/v1/emoji",
+        dataType: "json",
+        type: "get",
+        success: function(emojis_data) {
+          return $.each(emojis_data.emoji, function(i, emoji) {
+            var list_elm;
+            list_elm = $('<li class="col-xs-4 col-sm-3 col-md-3 text-center"></li>');
+            list_elm.append('<img src="' + emoji.image.replace("emoji/original", "emoji/px32").replace(".svg", ".png") + '">');
+            list_elm.append('<div>:' + emoji.code + ':</div>');
+            $("#catalog").append(list_elm);
+            if ((i + 1) % 4 === 0) {
+              $("#catalog").append('<div class="hidden-xs clearfix"></div>');
+            }
+            if ((i + 1) % 3 === 0) {
+              return $("#catalog").append('<div class="visible-xs clearfix"></div>');
+            }
+          });
+        },
+        error: function(emojis_data) {
+          console.log("error: load json");
+          return console.log(data);
+        }
+      });
+    });
+  }).call(this);
 
 }).call(this);
--- src/coffees/catalog/catalog.coffee
+++ src/coffees/catalog/catalog.coffee
@@ -1,17 +1,28 @@
-console.log 222
-name = "aaa"
-console.log name
+(->
+  $(document).ready ->
+    $.ajax
+      url: "https://www.emojidex.com/api/v1/emoji"
+      dataType: "json"
+      type: "get"
+      success: (emojis_data) ->
+        $.each emojis_data.emoji, (i, emoji) ->
+          list_elm = $('<li class="col-xs-4 col-sm-3 col-md-3 text-center"></li>')
+          list_elm.append '<img src="' + emoji.image.replace("emoji/original", "emoji/px32").replace(".svg", ".png") + '">'
+          list_elm.append '<div>:' + emoji.code + ':</div>'
 
-signal = "blue"
-switch signal
- when "red" then alert "stop"
- when "blue" then alert "go"
- else console.log "owari"
+          $("#catalog").append list_elm
 
-x=50
-if x <  30
-	console.log "OK"
-else if  x < 40
-	alert "NO"
-else
-	alert "sankusu"
+          if (i+1) % 4 is 0
+            $("#catalog").append '<div class="hidden-xs clearfix"></div>'
+
+          if (i+1) % 3 is 0
+            $("#catalog").append '<div class="visible-xs clearfix"></div>'
+
+
+      error: (emojis_data) ->
+        console.log "error: load json"
+        console.log data
+
+
+  return
+).call this
