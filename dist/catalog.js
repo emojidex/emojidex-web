1729456ad3dc545445b3337cb9035def6791d881
giurasu <giurasu1192.gmail.com>
2014-10-21 17:34:07 +0900

    end create catalog v0.5

--- dist/catalog.js
+++ dist/catalog.js
@@ -1,22 +1,29 @@
 (function() {
-  (function() {
-    $(document).ready(function() {
+  $(document).ready(function() {
+    var catalog_sets;
+    catalog_sets = ["emoji", "emojidex"];
+    return $.each(catalog_sets, function(i, set_name) {
       return $.ajax({
-        url: "https://www.emojidex.com/api/v1/emoji",
+        url: "https://www.emojidex.com/api/v1/users/" + set_name + "/emoji",
         dataType: "json",
         type: "get",
         success: function(emojis_data) {
-          return $.each(emojis_data.emoji, function(i, emoji) {
-            var list_elm;
-            list_elm = $('<li class="col-xs-4 col-sm-3 col-md-3 text-center"></li>');
-            list_elm.append('<img src="' + emoji.image.replace("emoji/original", "emoji/px32").replace(".svg", ".png") + '">');
+          console.dir(emojis_data);
+          return $.each(emojis_data.emoji, function(j, emoji) {
+            var emoji_name, list_elm;
+            emoji_name = emoji.code.replace(RegExp(" ", "g"), "_");
+            list_elm = $('<li class="mt-l col-xs-4 col-sm-3 col-md-2 text-center"></li>');
+            list_elm.append('<img class="img-responsive" src="http://s3-us-west-2.amazonaws.com/assets.emojidex.com/emoji/px128/' + emoji_name + '.png">');
             list_elm.append('<div>:' + emoji.code + ':</div>');
-            $("#catalog").append(list_elm);
-            if ((i + 1) % 4 === 0) {
-              $("#catalog").append('<div class="hidden-xs clearfix"></div>');
+            $("#catalog_" + set_name).append(list_elm);
+            if ((j + 1) % 3 === 0) {
+              $("#catalog_" + set_name).append('<div class="visible-xs clearfix"></div>');
             }
-            if ((i + 1) % 3 === 0) {
-              return $("#catalog").append('<div class="visible-xs clearfix"></div>');
+            if ((j + 1) % 4 === 0) {
+              $("#catalog_" + set_name).append('<div class="visible-sm clearfix"></div>');
+            }
+            if ((j + 1) % 6 === 0) {
+              return $("#catalog_" + set_name).append('<div class="visible-md visible-lg clearfix"></div>');
             }
           });
         },
@@ -26,6 +33,6 @@
         }
       });
     });
-  }).call(this);
+  });
 
 }).call(this);
