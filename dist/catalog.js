723f324bb7b77b0ef0533576109658a2fd0d6b79
snowsunny <snowsunny1107@gmail.com>
2014-10-24 18:04:19 +0900

    wip: fix catalog

--- dist/catalog.js
+++ dist/catalog.js
@@ -1,5 +1,5 @@
 (function() {
-  var get_categrized_emojis_data;
+  var get_categrized_emojis_data, set_emoji_list;
 
   get_categrized_emojis_data = function(emojis_data) {
     var categrized_data, emoji, _i, _len;
@@ -25,61 +25,66 @@
     return categrized_data;
   };
 
+  set_emoji_list = function(emojis_data) {
+    var categorized_emojis_data, clearfix_data_array, tab_content, tab_list;
+    clearfix_data_array = [
+      {
+        split_num: 3,
+        visible_size: "visible-xs"
+      }, {
+        split_num: 4,
+        visible_size: "visible-sm"
+      }, {
+        split_num: 6,
+        visible_size: "visible-md visible-lg"
+      }
+    ];
+    categorized_emojis_data = get_categrized_emojis_data(emojis_data.emoji);
+    tab_list = $('<ul class="nav nav-tabs"></ul>');
+    tab_content = $("<div class='tab-content'></div>");
+    $.each(categorized_emojis_data, function(category_name, category_emojis) {
+      var emoji_list, tab_pane;
+      tab_pane = $("<div class='tab-pane" + (tab_list[0].children.length === 0 ? " active" : "") + "' id='" + catalog_name + "-" + category_name + "'></div>");
+      tab_list.append(("<li class='" + (tab_list[0].children.length === 0 ? " active" : "") + "'><a href='#" + catalog_name + "-" + category_name + "' data-toggle='tab'>") + category_name + "</a></li>");
+      emoji_list = $("<ul class='list-unstyled'></ul>");
+      $.each(category_emojis, function(j, emoji) {
+        var emoji_name, list_elm;
+        emoji_name = emoji.code.replace(RegExp(" ", "g"), "_");
+        list_elm = $('<li class="mt-l col-xs-4 col-sm-3 col-md-2 text-center"></li>');
+        list_elm.append('<img class="img-responsive" src="http://s3-us-west-2.amazonaws.com/assets.emojidex.com/emoji/px128/' + emoji_name + '.png">');
+        list_elm.append('<div>:' + emoji.code + ':</div>');
+        emoji_list.append(list_elm);
+        return $.each(clearfix_data_array, function(k, data) {
+          if ((j + 1) % data.split_num === 0) {
+            return emoji_list.append('<div class="' + data.visible_size + ' clearfix"></div>');
+          }
+        });
+      });
+      tab_pane.append(emoji_list);
+      return tab_content.append(tab_pane);
+    });
+    $("#" + catalog_name + "-category-tabs").append(tab_list);
+    return $("#" + catalog_name + "-category-tabs").append(tab_content);
+  };
+
   $(document).ready(function() {
-    var catalog_sets;
-    catalog_sets = ["emoji", "emojidex"];
-    return $.each(catalog_sets, function(i, catalog_name) {
-      return $.ajax({
-        url: "https://www.emojidex.com/api/v1/users/" + catalog_name + "/emoji",
+    var usernames, _i, _len, _results;
+    usernames = ["emoji", "emojidex"];
+    _results = [];
+    for (_i = 0, _len = user.length; _i < _len; _i++) {
+      usernames = user[_i];
+      _results.push($.ajax({
+        url: "https://www.emojidex.com/api/v1/users/" + user + "/emoji",
         dataType: "json",
         type: "get",
-        success: function(emojis_data) {
-          var categorized_emojis_data, clearfix_data_array, tab_content, tab_list;
-          categorized_emojis_data = get_categrized_emojis_data(emojis_data.emoji);
-          clearfix_data_array = [
-            {
-              split_num: 3,
-              visible_size: "visible-xs"
-            }, {
-              split_num: 4,
-              visible_size: "visible-sm"
-            }, {
-              split_num: 6,
-              visible_size: "visible-md visible-lg"
-            }
-          ];
-          tab_list = $('<ul class="nav nav-tabs"></ul>');
-          tab_content = $("<div class='tab-content'></div>");
-          $.each(categorized_emojis_data, function(category_name, category_emojis) {
-            var emoji_list, tab_pane;
-            tab_pane = $("<div class='tab-pane" + (tab_list[0].children.length === 0 ? " active" : "") + "' id='" + catalog_name + "-" + category_name + "'></div>");
-            tab_list.append(("<li class='" + (tab_list[0].children.length === 0 ? " active" : "") + "'><a href='#" + catalog_name + "-" + category_name + "' data-toggle='tab'>") + category_name + "</a></li>");
-            emoji_list = $("<ul class='list-unstyled'></ul>");
-            $.each(category_emojis, function(j, emoji) {
-              var emoji_name, list_elm;
-              emoji_name = emoji.code.replace(RegExp(" ", "g"), "_");
-              list_elm = $('<li class="mt-l col-xs-4 col-sm-3 col-md-2 text-center"></li>');
-              list_elm.append('<img class="img-responsive" src="http://s3-us-west-2.amazonaws.com/assets.emojidex.com/emoji/px128/' + emoji_name + '.png">');
-              list_elm.append('<div>:' + emoji.code + ':</div>');
-              emoji_list.append(list_elm);
-              return $.each(clearfix_data_array, function(k, data) {
-                if ((j + 1) % data.split_num === 0) {
-                  return emoji_list.append('<div class="' + data.visible_size + ' clearfix"></div>');
-                }
-              });
-            });
-            tab_pane.append(emoji_list);
-            return tab_content.append(tab_pane);
-          });
-          $("#" + catalog_name + "-category-tabs").append(tab_list);
-          return $("#" + catalog_name + "-category-tabs").append(tab_content);
-        },
+        success: function(emojis_data) {},
         error: function(emojis_data) {
           console.log("error: load json");
           return console.log(data);
         }
-      });
-    });
+      }));
+    }
+    return _results;
   });
 
 }).call(this);
--- src/coffees/catalog/catalog.coffee
+++ src/coffees/catalog/catalog.coffee
@@ -16,48 +16,50 @@ get_categrized_emojis_data = (emojis_data) ->
 
   return categrized_data
 
+set_emoji_list = (emojis_data) ->
+  clearfix_data_array = [
+    {split_num: 3, visible_size: "visible-xs"}
+    {split_num: 4, visible_size: "visible-sm"}
+    {split_num: 6, visible_size: "visible-md visible-lg"}
+  ]
+
+  categorized_emojis_data = get_categrized_emojis_data emojis_data.emoji
+
+  tab_list = $('<ul class="nav nav-tabs"></ul>')
+  tab_content = $("<div class='tab-content'></div>")
+  $.each categorized_emojis_data, (category_name, category_emojis) ->
+    tab_pane = $("<div class='tab-pane#{if tab_list[0].children.length is 0 then " active" else ""}' id='#{catalog_name}-#{category_name}'></div>")
+    tab_list.append "<li class='#{if tab_list[0].children.length is 0 then " active" else ""}'><a href='##{catalog_name}-#{category_name}' data-toggle='tab'>" + category_name + "</a></li>"
+
+    emoji_list = $("<ul class='list-unstyled'></ul>")
+    $.each category_emojis, (j, emoji) ->
+      emoji_name = emoji.code.replace RegExp(" ", "g"), "_"
+      list_elm = $('<li class="mt-l col-xs-4 col-sm-3 col-md-2 text-center"></li>')
+      list_elm.append '<img class="img-responsive" src="http://s3-us-west-2.amazonaws.com/assets.emojidex.com/emoji/px128/' + emoji_name + '.png">'
+      list_elm.append '<div>:' + emoji.code + ':</div>'
+      emoji_list.append list_elm
+
+      $.each clearfix_data_array, (k, data) ->
+        if (j+1) % data.split_num is 0
+          emoji_list.append '<div class="' + data.visible_size + ' clearfix"></div>'
+
+     tab_pane.append emoji_list
+     tab_content.append tab_pane
+
+  $("##{catalog_name}-category-tabs").append tab_list
+  $("##{catalog_name}-category-tabs").append tab_content
+
 $(document).ready ->
-  catalog_sets = ["emoji", "emojidex"]
+  usernames = ["emoji", "emojidex"]
 
-  $.each catalog_sets, (i, catalog_name) ->
-
+  for usernames in user
     $.ajax
-      url: "https://www.emojidex.com/api/v1/users/" + catalog_name + "/emoji"
+      url: "https://www.emojidex.com/api/v1/users/" + user + "/emoji"
       dataType: "json"
       type: "get"
 
       success: (emojis_data) ->
-        categorized_emojis_data = get_categrized_emojis_data emojis_data.emoji
-
-        clearfix_data_array = [
-          {split_num: 3, visible_size: "visible-xs"}
-          {split_num: 4, visible_size: "visible-sm"}
-          {split_num: 6, visible_size: "visible-md visible-lg"}
-        ]
-
-        tab_list = $('<ul class="nav nav-tabs"></ul>')
-        tab_content = $("<div class='tab-content'></div>")
-        $.each categorized_emojis_data, (category_name, category_emojis) ->
-          tab_pane = $("<div class='tab-pane#{if tab_list[0].children.length is 0 then " active" else ""}' id='#{catalog_name}-#{category_name}'></div>")
-          tab_list.append "<li class='#{if tab_list[0].children.length is 0 then " active" else ""}'><a href='##{catalog_name}-#{category_name}' data-toggle='tab'>" + category_name + "</a></li>"
-
-          emoji_list = $("<ul class='list-unstyled'></ul>")
-          $.each category_emojis, (j, emoji) ->
-            emoji_name = emoji.code.replace RegExp(" ", "g"), "_"
-            list_elm = $('<li class="mt-l col-xs-4 col-sm-3 col-md-2 text-center"></li>')
-            list_elm.append '<img class="img-responsive" src="http://s3-us-west-2.amazonaws.com/assets.emojidex.com/emoji/px128/' + emoji_name + '.png">'
-            list_elm.append '<div>:' + emoji.code + ':</div>'
-            emoji_list.append list_elm
-
-            $.each clearfix_data_array, (k, data) ->
-              if (j+1) % data.split_num is 0
-                emoji_list.append '<div class="' + data.visible_size + ' clearfix"></div>'
-
-           tab_pane.append emoji_list
-           tab_content.append tab_pane
-
-        $("##{catalog_name}-category-tabs").append tab_list
-        $("##{catalog_name}-category-tabs").append tab_content
+        
 
       error: (emojis_data) ->
         console.log "error: load json"
