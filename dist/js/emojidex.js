797c25fd135bea43c1994ecfc3d62600fee6f3c5
Rika Yoshida <nazuki@genshin.org>
2015-11-25 17:07:52 +0900

    add emoji buttons [wip]

--- dist/js/emojidex.js
+++ dist/js/emojidex.js
@@ -240,16 +240,29 @@ var Window=null;!function(a){"use strict";Window=function(b){b=b||{};var c={sele
       body = '';
       return $(element).click(function(e) {
         var tab_content, tab_list;
-        tab_list = $('<ul class="nav nav-tabs"></ul>');
+        tab_list = $('<ul class="nav nav-pills"></ul>');
         tab_content = $('<div class="tab-content"></div>');
-        return _this.ec.Categories.sync(function(categories) {
-          var category, _i, _len;
-          console.dir(categories);
-          for (_i = 0, _len = categories.length; _i < _len; _i++) {
-            category = categories[_i];
-            tab_list.append("<li class='" + (tab_list[0].children.length === 0 ? " active" : "") + "'><a href='#" + category.name + "' data-toggle='tab'>" + category.name[0] + "</a></li>");
+        return _this.ec.Search.search("face", function(result_emoji) {
+          var emoji, emoji_list, _i, _len;
+          console.log(result_emoji);
+          console.dir(_this);
+          emoji_list = $('<div class="emoji_list"></div>');
+          for (_i = 0, _len = result_emoji.length; _i < _len; _i++) {
+            emoji = result_emoji[_i];
+            emoji_list.append("<button class='btn btn-default col-xs-2'><img src='" + _this.ec.cdn_url + "px32/" + (emoji.code.replace(/\s/g, '_')) + ".png'></img></button>");
           }
-          return _this.setWindow(tab_list);
+          console.dir(_this.ec);
+          return _this.ec.Categories.sync(function(categories) {
+            var category, _j, _len1;
+            console.dir(categories);
+            for (_j = 0, _len1 = categories.length; _j < _len1; _j++) {
+              category = categories[_j];
+              tab_list.append("<li class='" + (tab_list[0].children.length === 0 ? " active" : "") + "'><a href='#" + category.name + "' data-toggle='pill'>" + category.name + "</a></li>");
+              tab_content.append("<div class='tab-pane " + (tab_content[0].children.length === 0 ? " active" : "") + "' id='" + category.name + "'></div>");
+              tab_content.append(emoji_list);
+            }
+            return _this.setWindow(tab_list.add(tab_content));
+          });
         });
       });
     };
--- src/coffee/emojidexPallet/components/pallet.coffee
+++ src/coffee/emojidexPallet/components/pallet.coffee
@@ -7,18 +7,31 @@ class Pallet
     body = ''
 
     $(element).click (e) =>
-      tab_list = $ '<ul class="nav nav-tabs"></ul>'
+      tab_list = $ '<ul class="nav nav-pills"></ul>'
       tab_content = $ '<div class="tab-content"></div>'
 
-      @ec.Categories.sync (categories) =>
-        console.dir categories
-        for category in categories
-          # tab_pane = $("<div class='tab-pane#{if tab_list[0].children.length is 0 then " active" else ""}' id='#{category_name}'></div>")
-          tab_list.append "<li class='#{if tab_list[0].children.length is 0 then " active" else ""}'><a href='##{category.name}' data-toggle='tab'>#{category.name[0]}</a></li>"
+      @ec.Search.search "face", (result_emoji) =>
+        console.log result_emoji
+        console.dir @
+
+        emoji_list = $ '<div class="emoji_list"></div>'
+        for emoji in result_emoji
+          # console.log emoji
+          emoji_list.append "<button class='btn btn-default col-xs-2'><img src='#{@ec.cdn_url}px32/#{emoji.code.replace /\s/g, '_'}.png'></img></button>"
+
+        console.dir @ec
+        @ec.Categories.sync (categories) =>
+          console.dir categories
+          for category in categories
+            # tab_pane = $("<div class='tab-pane#{if tab_list[0].children.length is 0 then " active" else ""}' id='#{category_name}'></div>")
+            tab_list.append "<li class='#{if tab_list[0].children.length is 0 then " active" else ""}'><a href='##{category.name}' data-toggle='pill'>#{category.name}</a></li>"
+            tab_content.append "<div class='tab-pane #{if tab_content[0].children.length is 0 then " active" else ""}' id='#{category.name}'></div>"
 
 
+            tab_content.append emoji_list
 
-        @setWindow(tab_list)
+
+          @setWindow tab_list.add tab_content
 
 
   setWindow: (body) ->
