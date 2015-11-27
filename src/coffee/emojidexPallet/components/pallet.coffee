1c0951621bafb3a63a4eff5520c3043e06fccdf5
Rika Yoshida <nazuki@genshin.org>
2015-11-27 17:55:07 +0900

    add search [wip]

--- src/coffee/emojidexPallet/components/pallet.coffee
+++ src/coffee/emojidexPallet/components/pallet.coffee
@@ -3,42 +3,42 @@ class Pallet
     @ec = new EmojidexClient
     @clipboard = new Clipboard '.emoji-btn'
 
-    @prev_tab_content = $ '<div class="tab-pane" id="prev"></div>'
-    @next_tab_content = $ '<div class="tab-pane" id="next"></div>'
+    @search_tab_content = $ '<div class="tab-pane" id="search_tab"></div>'
     @setPallet @plugin.element
 
   setPallet: (element) ->
     body = ''
 
     $(element).click (e) =>
+      search_input = '<li><div class="input-group"><label class="sr-only" for="search">検索</label><input type="text" name="search" id="sidebar-emoji-search-input" class="form-control" placeholder="検索"><span class="input-group-btn"><button type="submit" class="btn btn-primary" id="sidebar-emoji-search-submit"><span class="glyphicon glyphicon-search"></span></button></span></div></li>'
+
       tab_list = $ '<ul class="nav nav-pills"></ul>'
       tab_content = $ '<div class="tab-content"></div>'
 
-      @ec.Search.search "face", (result_emoji) =>
-        emoji_list = $ '<div class="emoji_list"></div>'
-        for emoji in result_emoji
-          emoji_list.append "<button class='btn btn-default col-xs-1 emoji-btn' data-clipboard-text=':#{emoji.code.replace /\s/g, '_'}:'><img src='#{@ec.cdn_url}px32/#{emoji.code.replace /\s/g, '_'}.png'></img></button>"
+      @ec.Categories.sync (categories) =>
+        for category in categories
+          tab_list.append "<li class='#{if tab_list[0].children.length is 0 then " active" else ""}'><a href='##{category.name}' data-toggle='pill'>#{category.name}</a></li>"
+          tab_content.append "<div class='tab-pane #{if tab_content[0].children.length is 0 then " active" else ""}' id='#{category.name}'>#{category.name}</div>"
+          # TODO: カテゴライズされた絵文字の追加
 
-        @ec.Categories.sync (categories) =>
-          # console.dir categories
-          for category in categories
-            # tab_pane = $("<div class='tab-pane#{if tab_list[0].children.length is 0 then " active" else ""}' id='#{category_name}'></div>")
-            tab_list.append "<li class='#{if tab_list[0].children.length is 0 then " active" else ""}'><a href='##{category.name}' data-toggle='pill'>#{category.name}</a></li>"
-            tab_content.append "<div class='tab-pane #{if tab_content[0].children.length is 0 then " active" else ""}' id='#{category.name}'>#{emoji_list.html()}</div>"
+        tab_list.append search_input
+        tab_list.append $("<li class=''><a hreh='#search_tab' data-toggle='pill'> 《 </a></li>").click =>
+          @ec.Search.prev()
+        tab_list.append $("<li class=''><a href='#search_tab' data-toggle='pill'>search</a></li>").click =>
+          @search()
+        tab_list.append $("<li class=''><a hreh='#search_tab' data-toggle='pill'> 》 </a></li>").click =>
+          @ec.Search.next()
+        tab_content.append @search_tab_content
 
-          tab_list.append $("<li class=''><a href='#prev' data-toggle='pill'>prev</a></li>").click (e) =>
-            $('.prev-search-emoji').remove()
-            @prev_tab_content.append "<div class='prev-search-emoji'>#{new Date().getSeconds()}</div>"
+        @setWindow tab_list.add tab_content
 
-          tab_list.append $("<li class=''><a href='#next' data-toggle='pill'>next</a></li>").click (e) =>
-            # @ec.Search.next()
-            $('.next-search-emoji').remove()
-            @next_tab_content.append "<div class='next-search-emoji'>#{new Date().getSeconds()}</div>"
-
-          tab_content.append @prev_tab_content
-          tab_content.append @next_tab_content
-
-          @setWindow tab_list.add tab_content
+  search: ->
+    @ec.Search.search "face", (result_emoji) =>
+      $('.serach_emoji_list').remove()
+      search_emoji_list = $ '<div class="serach_emoji_list"></div>'
+      for emoji in result_emoji
+        search_emoji_list.append "<button class='btn btn-default col-xs-1 emoji-btn' data-clipboard-text=':#{emoji.code.replace /\s/g, '_'}:'><img src='#{@ec.cdn_url}px32/#{emoji.code.replace /\s/g, '_'}.png'></img></button>"
+      @search_tab_content.append search_emoji_list
 
   setWindow: (body) ->
     template = $("
@@ -61,7 +61,3 @@ class Pallet
       template: template
       title: 'emoji pallet'
       bodyContent: body
-
-  # pagePrev: () ->
-  #   console.log 'push prev'
-  #   # @prev_tab_content.append "<div>ccc</div>"
--- src/compiled_js/emojidexPallet.js
+++ src/compiled_js/emojidexPallet.js
@@ -46,8 +46,7 @@
       this.plugin = plugin;
       this.ec = new EmojidexClient;
       this.clipboard = new Clipboard('.emoji-btn');
-      this.prev_tab_content = $('<div class="tab-pane" id="prev"></div>');
-      this.next_tab_content = $('<div class="tab-pane" id="next"></div>');
+      this.search_tab_content = $('<div class="tab-pane" id="search_tab"></div>');
       this.setPallet(this.plugin.element);
     }
 
@@ -56,36 +55,44 @@
         _this = this;
       body = '';
       return $(element).click(function(e) {
-        var tab_content, tab_list;
+        var search_input, tab_content, tab_list;
+        search_input = '<li><div class="input-group"><label class="sr-only" for="search">検索</label><input type="text" name="search" id="sidebar-emoji-search-input" class="form-control" placeholder="検索"><span class="input-group-btn"><button type="submit" class="btn btn-primary" id="sidebar-emoji-search-submit"><span class="glyphicon glyphicon-search"></span></button></span></div></li>';
         tab_list = $('<ul class="nav nav-pills"></ul>');
         tab_content = $('<div class="tab-content"></div>');
-        return _this.ec.Search.search("face", function(result_emoji) {
-          var emoji, emoji_list, _i, _len;
-          emoji_list = $('<div class="emoji_list"></div>');
-          for (_i = 0, _len = result_emoji.length; _i < _len; _i++) {
-            emoji = result_emoji[_i];
-            emoji_list.append("<button class='btn btn-default col-xs-1 emoji-btn' data-clipboard-text=':" + (emoji.code.replace(/\s/g, '_')) + ":'><img src='" + _this.ec.cdn_url + "px32/" + (emoji.code.replace(/\s/g, '_')) + ".png'></img></button>");
+        return _this.ec.Categories.sync(function(categories) {
+          var category, _i, _len;
+          for (_i = 0, _len = categories.length; _i < _len; _i++) {
+            category = categories[_i];
+            tab_list.append("<li class='" + (tab_list[0].children.length === 0 ? " active" : "") + "'><a href='#" + category.name + "' data-toggle='pill'>" + category.name + "</a></li>");
+            tab_content.append("<div class='tab-pane " + (tab_content[0].children.length === 0 ? " active" : "") + "' id='" + category.name + "'>" + category.name + "</div>");
           }
-          return _this.ec.Categories.sync(function(categories) {
-            var category, _j, _len1;
-            for (_j = 0, _len1 = categories.length; _j < _len1; _j++) {
-              category = categories[_j];
-              tab_list.append("<li class='" + (tab_list[0].children.length === 0 ? " active" : "") + "'><a href='#" + category.name + "' data-toggle='pill'>" + category.name + "</a></li>");
-              tab_content.append("<div class='tab-pane " + (tab_content[0].children.length === 0 ? " active" : "") + "' id='" + category.name + "'>" + (emoji_list.html()) + "</div>");
-            }
-            tab_list.append($("<li class=''><a href='#prev' data-toggle='pill'>prev</a></li>").click(function(e) {
-              $('.prev-search-emoji').remove();
-              return _this.prev_tab_content.append("<div class='prev-search-emoji'>" + (new Date().getSeconds()) + "</div>");
-            }));
-            tab_list.append($("<li class=''><a href='#next' data-toggle='pill'>next</a></li>").click(function(e) {
-              $('.next-search-emoji').remove();
-              return _this.next_tab_content.append("<div class='next-search-emoji'>" + (new Date().getSeconds()) + "</div>");
-            }));
-            tab_content.append(_this.prev_tab_content);
-            tab_content.append(_this.next_tab_content);
-            return _this.setWindow(tab_list.add(tab_content));
-          });
+          tab_list.append(search_input);
+          tab_list.append($("<li class=''><a hreh='#search_tab' data-toggle='pill'> 《 </a></li>").click(function() {
+            return _this.ec.Search.prev();
+          }));
+          tab_list.append($("<li class=''><a href='#search_tab' data-toggle='pill'>search</a></li>").click(function() {
+            return _this.search();
+          }));
+          tab_list.append($("<li class=''><a hreh='#search_tab' data-toggle='pill'> 》 </a></li>").click(function() {
+            return _this.ec.Search.next();
+          }));
+          tab_content.append(_this.search_tab_content);
+          return _this.setWindow(tab_list.add(tab_content));
         });
+      });
+    };
+
+    Pallet.prototype.search = function() {
+      var _this = this;
+      return this.ec.Search.search("face", function(result_emoji) {
+        var emoji, search_emoji_list, _i, _len;
+        $('.serach_emoji_list').remove();
+        search_emoji_list = $('<div class="serach_emoji_list"></div>');
+        for (_i = 0, _len = result_emoji.length; _i < _len; _i++) {
+          emoji = result_emoji[_i];
+          search_emoji_list.append("<button class='btn btn-default col-xs-1 emoji-btn' data-clipboard-text=':" + (emoji.code.replace(/\s/g, '_')) + ":'><img src='" + _this.ec.cdn_url + "px32/" + (emoji.code.replace(/\s/g, '_')) + ".png'></img></button>");
+        }
+        return _this.search_tab_content.append(search_emoji_list);
       });
     };
 
