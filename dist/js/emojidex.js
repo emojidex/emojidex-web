17aed05d601cf46c699b2c6565c8368652d98be6
Rika Yoshida <nazuki@genshin.org>
2015-12-03 17:58:11 +0900

    [wip] modify search-tab layout

--- dist/js/emojidex.js
+++ dist/js/emojidex.js
@@ -245,45 +245,47 @@ var Window=null;!function(a){"use strict";Window=function(b){b=b||{};var c={sele
       this.plugin = plugin;
       this.ec = new EmojidexClient;
       this.clipboard = new Clipboard('.emoji-btn');
-      this.search_tab_content = $('<div class="tab-pane" id="search_tab"></div>');
       this.setPallet(this.plugin.element);
+      this.can_create_window = true;
     }
 
     Pallet.prototype.setPallet = function(element) {
       var _this = this;
       return $(element).click(function(e) {
-        var search_btn, search_input, tab_content, tab_list;
-        search_input = $('<div class="input-group"><input type="text" name="search" id="pallet-emoji-search-input" class="form-control" placeholder="検索"><span class="input-group-btn"></span></div>');
-        search_btn = $('<button type="submit" class="btn btn-primary" id="pallet-emoji-search-submit"><span class="glyphicon glyphicon-search"></span></button>');
-        search_btn.click(function() {
-          return console.log(111);
-        });
-        search_input.find('.input-group-btn').append(search_btn);
-        _this.search_tab_content.append(search_input);
-        console.log($('#pallet-emoji-search-submit'));
-        tab_list = $('<ul class="nav nav-pills"></ul>');
-        tab_content = $('<div class="tab-content"></div>');
-        return _this.ec.Categories.sync(function(categories) {
-          var category, _i, _len;
-          for (_i = 0, _len = categories.length; _i < _len; _i++) {
-            category = categories[_i];
-            tab_list.append("<li class='" + (tab_list[0].children.length === 0 ? " active" : "") + "'><a href='#" + category.name + "' data-toggle='pill'>" + category.name + "</a></li>");
-            tab_content.append("<div class='tab-pane " + (tab_content[0].children.length === 0 ? " active" : "") + "' id='" + category.name + "'>" + category.name + "</div>");
-          }
-          tab_list.append($('<li class=""><a href="#search_tab" data-toggle="pill">Search</a></li>').click(function() {
-            console.log('search');
-            return _this.search();
-          }));
-          tab_content.append(_this.search_tab_content);
-          return _this.setWindow(tab_list.add(tab_content));
-        });
+        var search_btn, tab_content, tab_list;
+        if (_this.can_create_window) {
+          _this.can_create_window = false;
+          _this.search_tab_content = $('<div class="tab-pane" id="search_tab"><div class="input-group"><input type="text" name="search" id="pallet-emoji-search-input" class="form-control" placeholder="検索"><span class="input-group-btn"></span></div></div>');
+          search_btn = $('<button type="submit" class="btn btn-primary" id="pallet-emoji-search-submit"><span class="glyphicon glyphicon-search"></span></button>');
+          search_btn.click(function() {
+            var search_word;
+            search_word = $('#pallet-emoji-search-input').val();
+            if (search_word.length > 0) {
+              return _this.search(search_word);
+            }
+          });
+          _this.search_tab_content.find('.input-group-btn').append(search_btn);
+          tab_list = $('<ul class="nav nav-pills"></ul>');
+          tab_content = $('<div class="tab-content"></div>');
+          return _this.ec.Categories.sync(function(categories) {
+            var category, _i, _len;
+            for (_i = 0, _len = categories.length; _i < _len; _i++) {
+              category = categories[_i];
+              tab_list.append("<li class='" + (tab_list[0].children.length === 0 ? " active" : "") + "'><a href='#" + category.name + "' data-toggle='pill'>" + category.name + "</a></li>");
+              tab_content.append("<div class='tab-pane " + (tab_content[0].children.length === 0 ? " active" : "") + "' id='" + category.name + "'>" + category.name + "</div>");
+            }
+            tab_list.append("<li class=''><a href='#search_tab' data-toggle='pill'>Search</a></li>");
+            tab_content.append(_this.search_tab_content);
+            return _this.setWindow(tab_list.add(tab_content));
+          });
+        }
       });
     };
 
-    Pallet.prototype.search = function() {
+    Pallet.prototype.search = function(search_word) {
       var _this = this;
-      return this.ec.Search.search("face", function(result_emoji) {
-        var emoji, pagination, search_emoji_list, _i, _len;
+      return this.ec.Search.search(search_word, function(result_emoji) {
+        var emoji, max_page, now_page, pagination, search_emoji_list, _i, _len;
         $('.serach_emoji_list').remove();
         $('.search_pagination').remove();
         search_emoji_list = $('<div class="serach_emoji_list"></div>');
@@ -292,20 +294,31 @@ var Window=null;!function(a){"use strict";Window=function(b){b=b||{};var c={sele
           search_emoji_list.append("<button class='btn btn-default col-xs-1 emoji-btn' data-clipboard-text=':" + (emoji.code.replace(/\s/g, '_')) + ":'><img src='" + _this.ec.cdn_url + "px32/" + (emoji.code.replace(/\s/g, '_')) + ".png'></img></button>");
         }
         _this.search_tab_content.append(search_emoji_list);
-        pagination = $('<div class="search_pagination"></div>');
-        pagination.append($('<button>《 </button>').click(function() {
-          return _this.ec.Search.prev();
+        now_page = 2;
+        max_page = 10;
+        pagination = $('<div class="search_pagination row"></div>');
+        pagination.append($('<button class="btn btn-link col-xs-1 col-xs-offset-4">《 </button>').click(function() {
+          if (now_page !== 1) {
+            return _this.ec.Search.prev();
+          }
         }));
-        pagination.append($('<button> 》 </button>').click(function() {
-          return _this.ec.Search.next();
+        pagination.append($("<span class='col-xs-2 col-xs-offset-1'>" + now_page + " / " + max_page + "</span>"));
+        pagination.append($('<button class="btn btn-link col-xs-1"> 》 </button>').click(function() {
+          if (now_page < max_page) {
+            return _this.ec.Search.next();
+          }
         }));
         return _this.search_tab_content.append(pagination);
       });
     };
 
     Pallet.prototype.setWindow = function(body) {
-      var ep, template;
-      template = $("      <div id='templage'>        <div class='window emoji-pallet'>          <div class='window-header'>            <button type='button' class='close' data-dismiss='window' aria-hidden='true'>              x            </button>            <h4 class='window-title text-primary'>            </h4>          </div>          <div class='window-body'>          </div>        </div>      </div>    ").html();
+      var ep, template,
+        _this = this;
+      template = $("      <div class='window emoji-pallet'>        <div class='window-header'>          <button type='button' class='close' data-dismiss='window' aria-hidden='true'>            x          </button>          <h4 class='window-title text-primary'>          </h4>        </div>        <div class='window-body'>        </div>      </div>    ");
+      template.find('.close').click(function() {
+        return _this.can_create_window = true;
+      });
       return ep = new Window({
         template: template,
         title: 'emoji pallet',
--- src/coffee/emojidexPallet/components/pallet.coffee
+++ src/coffee/emojidexPallet/components/pallet.coffee
@@ -2,48 +2,37 @@ class Pallet
   constructor: (@plugin) ->
     @ec = new EmojidexClient
     @clipboard = new Clipboard '.emoji-btn'
-
-    @search_tab_content = $ '<div class="tab-pane" id="search_tab"></div>'
     @setPallet @plugin.element
+    @can_create_window = true
 
   setPallet: (element) ->
     $(element).click (e) =>
-      search_input = $ '<div class="input-group"><input type="text" name="search" id="pallet-emoji-search-input" class="form-control" placeholder="検索"><span class="input-group-btn"></span></div>'
+      if @can_create_window
+        @can_create_window = false
+        @search_tab_content = $ '<div class="tab-pane" id="search_tab"><div class="input-group"><input type="text" name="search" id="pallet-emoji-search-input" class="form-control" placeholder="検索"><span class="input-group-btn"></span></div></div>'
+        search_btn = $ '<button type="submit" class="btn btn-primary" id="pallet-emoji-search-submit"><span class="glyphicon glyphicon-search"></span></button>'
+        search_btn.click =>
+          search_word = $('#pallet-emoji-search-input').val()
+          if search_word.length > 0
+            @search(search_word)
+        @search_tab_content.find('.input-group-btn').append search_btn
 
-      search_btn = $ '<button type="submit" class="btn btn-primary" id="pallet-emoji-search-submit"><span class="glyphicon glyphicon-search"></span></button>'
-      search_btn.click ->
-        # debugger
-        console.log 111
+        tab_list = $ '<ul class="nav nav-pills"></ul>'
+        tab_content = $ '<div class="tab-content"></div>'
 
+        @ec.Categories.sync (categories) =>
+          for category in categories
+            tab_list.append "<li class='#{if tab_list[0].children.length is 0 then " active" else ""}'><a href='##{category.name}' data-toggle='pill'>#{category.name}</a></li>"
+            tab_content.append "<div class='tab-pane #{if tab_content[0].children.length is 0 then " active" else ""}' id='#{category.name}'>#{category.name}</div>"
+            # TODO: カテゴライズされた絵文字の追加
 
-      search_input.find('.input-group-btn').append search_btn
-      @search_tab_content.append search_input
-      console.log $('#pallet-emoji-search-submit')
-      tab_list = $ '<ul class="nav nav-pills"></ul>'
-      tab_content = $ '<div class="tab-content"></div>'
+          tab_list.append "<li class=''><a href='#search_tab' data-toggle='pill'>Search</a></li>"
+          tab_content.append @search_tab_content
 
-      @ec.Categories.sync (categories) =>
-        for category in categories
-          tab_list.append "<li class='#{if tab_list[0].children.length is 0 then " active" else ""}'><a href='##{category.name}' data-toggle='pill'>#{category.name}</a></li>"
-          tab_content.append "<div class='tab-pane #{if tab_content[0].children.length is 0 then " active" else ""}' id='#{category.name}'>#{category.name}</div>"
-          # TODO: カテゴライズされた絵文字の追加
+          @setWindow tab_list.add tab_content
 
-        tab_list.append $('<li class=""><a href="#search_tab" data-toggle="pill">Search</a></li>').click =>
-          console.log 'search'
-          @search()
-        tab_content.append @search_tab_content
-        # tab_list.append $("<li class=''><a hreh='#search_tab' data-toggle='pill'> 《 </a></li>").click =>
-        #   @ec.Search.prev()
-        # tab_list.append $("<li class=''><a href='#search_tab' data-toggle='pill'>search</a></li>").click =>
-        #   @search()
-        # tab_list.append $("<li class=''><a hreh='#search_tab' data-toggle='pill'> 》 </a></li>").click =>
-        #   @ec.Search.next()
-        # tab_content.append @search_tab_content
-
-        @setWindow tab_list.add tab_content
-
-  search: ->
-    @ec.Search.search "face", (result_emoji) =>
+  search: (search_word) ->
+    @ec.Search.search search_word, (result_emoji) =>
       $('.serach_emoji_list').remove()
       $('.search_pagination').remove()
 
@@ -52,30 +41,36 @@ class Pallet
         search_emoji_list.append "<button class='btn btn-default col-xs-1 emoji-btn' data-clipboard-text=':#{emoji.code.replace /\s/g, '_'}:'><img src='#{@ec.cdn_url}px32/#{emoji.code.replace /\s/g, '_'}.png'></img></button>"
       @search_tab_content.append search_emoji_list
 
-      pagination = $ '<div class="search_pagination"></div>'
-      pagination.append $('<button>《 </button>').click =>
-        @ec.Search.prev()
       # TODO: ページ番号
-      pagination.append $('<button> 》 </button>').click =>
-        @ec.Search.next()
+      now_page = 2
+      max_page = 10
+      pagination = $ '<div class="search_pagination row"></div>'
+      pagination.append $('<button class="btn btn-link col-xs-1 col-xs-offset-4">《 </button>').click =>
+        if now_page != 1
+          @ec.Search.prev()
+      pagination.append $("<span class='col-xs-2 col-xs-offset-1'>#{now_page} / #{max_page}</span>")
+      pagination.append $('<button class="btn btn-link col-xs-1"> 》 </button>').click =>
+        if now_page < max_page
+          @ec.Search.next()
       @search_tab_content.append pagination
 
   setWindow: (body) ->
     template = $("
-      <div id='templage'>
-        <div class='window emoji-pallet'>
-          <div class='window-header'>
-            <button type='button' class='close' data-dismiss='window' aria-hidden='true'>
-              x
-            </button>
-            <h4 class='window-title text-primary'>
-            </h4>
-          </div>
-          <div class='window-body'>
-          </div>
+      <div class='window emoji-pallet'>
+        <div class='window-header'>
+          <button type='button' class='close' data-dismiss='window' aria-hidden='true'>
+            x
+          </button>
+          <h4 class='window-title text-primary'>
+          </h4>
+        </div>
+        <div class='window-body'>
         </div>
       </div>
-    ").html()
+    ")
+
+    template.find('.close').click =>
+      @can_create_window = true
 
     ep = new Window
       template: template
