95bb189c965c379f773df1e20c787530e7d9cfde
Rika Yoshida <nazuki@genshin.org>
2015-12-07 15:03:23 +0900

    finish category tab

--- src/coffee/emojidexPallet/components/pallet.coffee
+++ src/coffee/emojidexPallet/components/pallet.coffee
@@ -3,6 +3,8 @@ class Pallet
     @ec = new EmojidexClient
     @clipboard = new Clipboard '.emoji-btn'
     @can_create_window = true
+    @show_first_tab = true
+    @tabs_emoji = []
 
     # start main --------
     @setPallet @plugin.element
@@ -31,9 +33,12 @@ class Pallet
 
         @ec.Categories.sync (categories) =>
           for category in categories
-            tab_list.append "<li class='#{if tab_list[0].children.length is 0 then " active" else ""}'><a href='##{category.name}' data-toggle='pill'>#{category.name}</a></li>"
-            tab_content.append "<div class='tab-pane #{if tab_content[0].children.length is 0 then " active" else ""}' id='#{category.name}'>#{category.name}</div>"
-            # TODO: カテゴライズされた絵文字の追加
+            tab_button = $ "<li id='tab-#{category.code}' data-code='#{category.code}' class='#{if tab_list[0].children.length is 0 then " active" else ""}'><a href='#tab-content-#{category.code}' data-toggle='pill'>#{category.name}</a></li>"
+            content_page = $ "<div class='tab-pane #{if tab_content[0].children.length is 0 then " active" else ""}' id='tab-content-#{category.code}'></div>"
+            tab_button.click (e) =>
+              @setCategory $(e.currentTarget).data 'code'
+            tab_list.append tab_button
+            tab_content.append content_page
 
           tab_list.append "<li class=''><a href='#search_tab' data-toggle='pill'>Search</a></li>"
           tab_content.append @search_tab_content
@@ -43,6 +48,55 @@ class Pallet
           @emoji_pallet.find('ul').after('<hr>')
 
           @setWindow @emoji_pallet
+          $("#tab-abstract").click()
+
+  setCategory: (category_name) ->
+    if @tabs_emoji.length && !@show_first_tab
+      for tab_data in @tabs_emoji
+        if tab_data.category_name is category_name
+          @ec.Categories.called_data = tab_data
+          break
+        else if tab_data is @tabs_emoji[@tabs_emoji.length - 1]
+          @setCategoryTabContent category_name
+    else
+      @show_first_tab = false
+      @setCategoryTabContent category_name
+
+  setCategoryTabContent: (category_name)->
+    @ec.Categories.getEmoji category_name, (result_emoji) =>
+      if @tabs_emoji.length
+        for tab_data, i in @tabs_emoji
+          if tab_data.category_name is category_name
+            @tabs_emoji[i] = @ec.Categories.called_data
+            break
+          else if tab_data is @tabs_emoji[@tabs_emoji.length - 1]
+            @tabs_emoji.push @ec.Categories.called_data
+      else
+        @tabs_emoji.push @ec.Categories.called_data
+
+      content_page = $ "#tab-content-#{category_name}"
+      content_page.find('.category-emoji-list').remove()
+      content_page.find('.category-pagination').remove()
+
+      category_emoji_list = $ '<div class="category-emoji-list clearfix"></div>'
+      for emoji in result_emoji
+        category_emoji_list.append @createEmojiButton(emoji.code)
+      content_page.append category_emoji_list
+
+      pagination = $ '<div class="category-pagination"><div class="text-center"><ul class="pagination"></ul></div></div>'
+      pagination.find('.pagination').append $('<li class="pallet-pager"><span>&laquo;</span></li>').click =>
+        @ec.Categories.prev()
+
+      cur_page = if @ec.Categories.meta.total_count == 0 then 0 else @ec.Categories.cur_page
+      max_page = Math.floor @ec.Categories.meta.total_count / @ec.options.limit
+      if @ec.Categories.meta.total_count % @ec.options.limit > 0
+        max_page++
+      pagination.find('.pagination').append $("<li class='disabled'><span>#{cur_page} / #{max_page}</span></li>")
+
+      pagination.find('.pagination').append $('<li class="pallet-pager"><span>&raquo;</span></li>').click =>
+        @ec.Categories.next()
+
+      content_page.append pagination
 
   search: (search_word) ->
     @ec.Search.search search_word, (result_emoji) =>
@@ -51,19 +105,25 @@ class Pallet
 
       search_emoji_list = $ '<div class="serach-emoji-list clearfix"></div>'
       for emoji in result_emoji
-        search_emoji_list.append "<button class='emoji-btn btn btn-default col-xs-2 col-sm-1' data-clipboard-text=':#{emoji.code.replace /\s/g, '_'}:'><img class='img-responsive center-block' src='#{@ec.cdn_url}px32/#{emoji.code.replace /\s/g, '_'}.png'></img></button>"
+        search_emoji_list.append @createEmojiButton(emoji.code)
       @search_tab_content.append search_emoji_list
 
-      # TODO: ページ番号
       pagination = $ '<div class="search-pagination"><div class="text-center"><ul class="pagination"></ul></div></div>'
-      pagination.find('.pagination').append $('<li><span>&laquo;</span></li>').click =>
-        if @ec.Search.cur_page > 1
-          @ec.Search.prev()
-      pagination.find('.pagination').append $("<li><span>#{@ec.Search.cur_page} / #{@ec.Search.count}</span></li>")
-      pagination.find('.pagination').append $('<li><span>&raquo;</span></li>').click =>
-        if @ec.Search.cur_page < @ec.Search.count
-          @ec.Search.next()
+      pagination.find('.pagination').append $('<li class="pallet-pager"><span>&laquo;</span></li>').click =>
+        @ec.Search.prev()
+
+      cur_page = if @ec.Search.meta.total_count == 0 then 0 else @ec.Search.cur_page
+      max_page = Math.floor @ec.Search.meta.total_count / @ec.options.limit
+      if @ec.Search.meta.total_count % @ec.options.limit > 0
+        max_page++
+      pagination.find('.pagination').append $("<li class='disabled'><span>#{cur_page} / #{max_page}</span></li>")
+
+      pagination.find('.pagination').append $('<li class="pallet-pager"><span>&raquo;</span></li>').click =>
+        @ec.Search.next()
       @search_tab_content.append pagination
+
+  createEmojiButton: (code) ->
+    return "<button class='emoji-btn btn btn-default col-xs-2 col-sm-1' data-clipboard-text=':#{code.replace /\s/g, '_'}:'><img alt='#{code}' title='#{code}' class='img-responsive center-block' src='#{@ec.cdn_url}px32/#{code.replace /\s/g, '_'}.png'></img></button>"
 
   setWindow: (body) ->
     template = $("
@@ -82,6 +142,7 @@ class Pallet
 
     template.find('.close').click =>
       @can_create_window = true
+      @show_first_tab = true
 
     ep = new Window
       template: template
--- src/compiled_js/emojidexPallet.js
+++ src/compiled_js/emojidexPallet.js
@@ -47,6 +47,8 @@
       this.ec = new EmojidexClient;
       this.clipboard = new Clipboard('.emoji-btn');
       this.can_create_window = true;
+      this.show_first_tab = true;
+      this.tabs_emoji = [];
       this.setPallet(this.plugin.element);
     }
 
@@ -78,49 +80,127 @@
           tab_list = $('<ul class="nav nav-pills"></ul>');
           tab_content = $('<div class="tab-content"></div>');
           return _this.ec.Categories.sync(function(categories) {
-            var category, _i, _len;
+            var category, content_page, tab_button, _i, _len;
             for (_i = 0, _len = categories.length; _i < _len; _i++) {
               category = categories[_i];
-              tab_list.append("<li class='" + (tab_list[0].children.length === 0 ? " active" : "") + "'><a href='#" + category.name + "' data-toggle='pill'>" + category.name + "</a></li>");
-              tab_content.append("<div class='tab-pane " + (tab_content[0].children.length === 0 ? " active" : "") + "' id='" + category.name + "'>" + category.name + "</div>");
+              tab_button = $("<li id='tab-" + category.code + "' data-code='" + category.code + "' class='" + (tab_list[0].children.length === 0 ? " active" : "") + "'><a href='#tab-content-" + category.code + "' data-toggle='pill'>" + category.name + "</a></li>");
+              content_page = $("<div class='tab-pane " + (tab_content[0].children.length === 0 ? " active" : "") + "' id='tab-content-" + category.code + "'></div>");
+              tab_button.click(function(e) {
+                return _this.setCategory($(e.currentTarget).data('code'));
+              });
+              tab_list.append(tab_button);
+              tab_content.append(content_page);
             }
             tab_list.append("<li class=''><a href='#search_tab' data-toggle='pill'>Search</a></li>");
             tab_content.append(_this.search_tab_content);
             _this.emoji_pallet = $('<div class="emoji-pallet"></div>');
             _this.emoji_pallet.append(tab_list.add(tab_content));
             _this.emoji_pallet.find('ul').after('<hr>');
-            return _this.setWindow(_this.emoji_pallet);
+            _this.setWindow(_this.emoji_pallet);
+            return $("#tab-abstract").click();
           });
         }
+      });
+    };
+
+    Pallet.prototype.setCategory = function(category_name) {
+      var tab_data, _i, _len, _ref, _results;
+      if (this.tabs_emoji.length && !this.show_first_tab) {
+        _ref = this.tabs_emoji;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          tab_data = _ref[_i];
+          if (tab_data.category_name === category_name) {
+            this.ec.Categories.called_data = tab_data;
+            break;
+          } else if (tab_data === this.tabs_emoji[this.tabs_emoji.length - 1]) {
+            _results.push(this.setCategoryTabContent(category_name));
+          } else {
+            _results.push(void 0);
+          }
+        }
+        return _results;
+      } else {
+        this.show_first_tab = false;
+        return this.setCategoryTabContent(category_name);
+      }
+    };
+
+    Pallet.prototype.setCategoryTabContent = function(category_name) {
+      var _this = this;
+      return this.ec.Categories.getEmoji(category_name, function(result_emoji) {
+        var category_emoji_list, content_page, cur_page, emoji, i, max_page, pagination, tab_data, _i, _j, _len, _len1, _ref;
+        if (_this.tabs_emoji.length) {
+          _ref = _this.tabs_emoji;
+          for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
+            tab_data = _ref[i];
+            if (tab_data.category_name === category_name) {
+              _this.tabs_emoji[i] = _this.ec.Categories.called_data;
+              break;
+            } else if (tab_data === _this.tabs_emoji[_this.tabs_emoji.length - 1]) {
+              _this.tabs_emoji.push(_this.ec.Categories.called_data);
+            }
+          }
+        } else {
+          _this.tabs_emoji.push(_this.ec.Categories.called_data);
+        }
+        content_page = $("#tab-content-" + category_name);
+        content_page.find('.category-emoji-list').remove();
+        content_page.find('.category-pagination').remove();
+        category_emoji_list = $('<div class="category-emoji-list clearfix"></div>');
+        for (_j = 0, _len1 = result_emoji.length; _j < _len1; _j++) {
+          emoji = result_emoji[_j];
+          category_emoji_list.append(_this.createEmojiButton(emoji.code));
+        }
+        content_page.append(category_emoji_list);
+        pagination = $('<div class="category-pagination"><div class="text-center"><ul class="pagination"></ul></div></div>');
+        pagination.find('.pagination').append($('<li class="pallet-pager"><span>&laquo;</span></li>').click(function() {
+          return _this.ec.Categories.prev();
+        }));
+        cur_page = _this.ec.Categories.meta.total_count === 0 ? 0 : _this.ec.Categories.cur_page;
+        max_page = Math.floor(_this.ec.Categories.meta.total_count / _this.ec.options.limit);
+        if (_this.ec.Categories.meta.total_count % _this.ec.options.limit > 0) {
+          max_page++;
+        }
+        pagination.find('.pagination').append($("<li class='disabled'><span>" + cur_page + " / " + max_page + "</span></li>"));
+        pagination.find('.pagination').append($('<li class="pallet-pager"><span>&raquo;</span></li>').click(function() {
+          return _this.ec.Categories.next();
+        }));
+        return content_page.append(pagination);
       });
     };
 
     Pallet.prototype.search = function(search_word) {
       var _this = this;
       return this.ec.Search.search(search_word, function(result_emoji) {
-        var emoji, pagination, search_emoji_list, _i, _len;
+        var cur_page, emoji, max_page, pagination, search_emoji_list, _i, _len;
         $('.serach-emoji-list').remove();
         $('.search-pagination').remove();
         search_emoji_list = $('<div class="serach-emoji-list clearfix"></div>');
         for (_i = 0, _len = result_emoji.length; _i < _len; _i++) {
           emoji = result_emoji[_i];
-          search_emoji_list.append("<button class='emoji-btn btn btn-default col-xs-2 col-sm-1' data-clipboard-text=':" + (emoji.code.replace(/\s/g, '_')) + ":'><img class='img-responsive center-block' src='" + _this.ec.cdn_url + "px32/" + (emoji.code.replace(/\s/g, '_')) + ".png'></img></button>");
+          search_emoji_list.append(_this.createEmojiButton(emoji.code));
         }
         _this.search_tab_content.append(search_emoji_list);
         pagination = $('<div class="search-pagination"><div class="text-center"><ul class="pagination"></ul></div></div>');
-        pagination.find('.pagination').append($('<li><span>&laquo;</span></li>').click(function() {
-          if (_this.ec.Search.cur_page > 1) {
-            return _this.ec.Search.prev();
-          }
+        pagination.find('.pagination').append($('<li class="pallet-pager"><span>&laquo;</span></li>').click(function() {
+          return _this.ec.Search.prev();
         }));
-        pagination.find('.pagination').append($("<li><span>" + _this.ec.Search.cur_page + " / " + _this.ec.Search.count + "</span></li>"));
-        pagination.find('.pagination').append($('<li><span>&raquo;</span></li>').click(function() {
-          if (_this.ec.Search.cur_page < _this.ec.Search.count) {
-            return _this.ec.Search.next();
-          }
+        cur_page = _this.ec.Search.meta.total_count === 0 ? 0 : _this.ec.Search.cur_page;
+        max_page = Math.floor(_this.ec.Search.meta.total_count / _this.ec.options.limit);
+        if (_this.ec.Search.meta.total_count % _this.ec.options.limit > 0) {
+          max_page++;
+        }
+        pagination.find('.pagination').append($("<li class='disabled'><span>" + cur_page + " / " + max_page + "</span></li>"));
+        pagination.find('.pagination').append($('<li class="pallet-pager"><span>&raquo;</span></li>').click(function() {
+          return _this.ec.Search.next();
         }));
         return _this.search_tab_content.append(pagination);
       });
+    };
+
+    Pallet.prototype.createEmojiButton = function(code) {
+      return "<button class='emoji-btn btn btn-default col-xs-2 col-sm-1' data-clipboard-text=':" + (code.replace(/\s/g, '_')) + ":'><img alt='" + code + "' title='" + code + "' class='img-responsive center-block' src='" + this.ec.cdn_url + "px32/" + (code.replace(/\s/g, '_')) + ".png'></img></button>";
     };
 
     Pallet.prototype.setWindow = function(body) {
@@ -128,7 +208,8 @@
         _this = this;
       template = $("      <div class='window emoji-pallet'>        <div class='window-header'>          <button type='button' class='close' data-dismiss='window' aria-hidden='true'>            x          </button>          <h4 class='window-title text-primary'>          </h4>        </div>        <div class='window-body'>        </div>      </div>    ");
       template.find('.close').click(function() {
-        return _this.can_create_window = true;
+        _this.can_create_window = true;
+        return _this.show_first_tab = true;
       });
       return ep = new Window({
         template: template,
