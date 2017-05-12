6ed7c8b9998da63236f61cb6c981a1857c5e5443
Rika Yoshida <nazuki@genshin.org>
2017-05-12 12:07:33 +0900

    [wip] create autocomplete plugin

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -229,8 +229,7 @@ gulp.task('concat-js', () => {
       'node_modules/bootstrap-sass/assets/javascripts/bootstrap/tab.js',
       'src/vendor/jquery-ui-1.12.1.custom/jquery-ui.min.js',
       'node_modules/emojidex-client/dist/js/emojidex-client.min.js',
-      'bower_components/Caret.js/dist/jquery.caret.min.js',
-      'bower_components/At.js/dist/js/jquery.atwho.min.js',
+      'node_modules/jquery-textcomplete/dist/jquery.textcomplete.min.js',
       'node_modules/clipboard/dist/clipboard.min.js',
       'build/js/**/*.js'
     ])
