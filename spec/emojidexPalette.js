f98239a5971eeebb0263312e4c2831d090cfca37
snowsunny <snowsunny1107@gmail.com>
2017-07-05 12:41:45 +0900

    add following tab spec

--- spec/emojidexPalette.js
+++ spec/emojidexPalette.js
@@ -263,6 +263,71 @@ describe("emojidexPalette", function() {
       $('#tab-content-user-history').find('.pagination .palette-pager')[0].click();
     });
 
+    // following tab --------
+    it('show following tab [Requires a user account and following user]', function(done) {
+      if (typeof user_info === 'undefined' || user_info === null) { pending(); }
+      $('#follow-following').watch({
+        id: "watcher",
+        properties: 'attr_class',
+        callback(data, i) {
+          expect($('#follow-following .users .btn').length).toBeTruthy();
+          removeWatch($('#follow-following'), 'watcher');
+          done();
+        }
+      });
+
+      $('#tab-user-following a').click();
+    });
+
+    it('show following user info [Requires a user account and following user]', function(done) {
+      if (typeof user_info === 'undefined' || user_info === null) { pending(); }
+      $('#follow-following .user-info').watch({
+        id: "watcher",
+        properties: 'attr_class',
+        callback(data, i) {
+          expect($('#follow-following .user-info.on .emoji-btn').length).toBeTruthy();
+          removeWatch($('.user-info'), 'watcher');
+          done();
+        }
+      });
+
+      $($('#follow-following .users .btn')[0]).click();
+    });
+
+    it('show following user emoji next [Requires a user account and following user]', function(done) {
+      if (typeof user_info === 'undefined' || user_info === null) { pending(); }
+      const selectorCurrentUserInfo= '#follow-following .user-info.on';
+      $(selectorCurrentUserInfo).watch({
+        id: "watcher",
+        properties: 'prop_innerHTML',
+        watchChildren: true,
+        callback(data, i) {
+          expect($(`${selectorCurrentUserInfo} .palette-num span`).text().charAt(0)).toBe('2');
+          removeWatch($('.user-info'), 'watcher');
+          done();
+        }
+      });
+
+      $($(`${selectorCurrentUserInfo} .palette-pager`)[1]).click();
+    });
+
+    it('show following user emoji previous [Requires a user account and following user]', function(done) {
+      if (typeof user_info === 'undefined' || user_info === null) { pending(); }
+      const selectorCurrentUserInfo= '#follow-following .user-info.on';
+      $(selectorCurrentUserInfo).watch({
+        id: "watcher",
+        properties: 'prop_innerHTML',
+        watchChildren: true,
+        callback(data, i) {
+          expect($(`${selectorCurrentUserInfo} .palette-num span`).text().charAt(0)).toBe('1');
+          removeWatch($('.user-info'), 'watcher');
+          done();
+        }
+      });
+
+      $($(`${selectorCurrentUserInfo} .palette-pager`)[0]).click();
+    });
+
    // it 'premium user can see the newest/popular emoji', (done) ->
    //   pending() unless premium_user_info?
    //   timer_option =
