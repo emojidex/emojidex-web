3aebce7256bbbd3b338c18209a573f8963917b8d
snowsunny <snowsunny1107@gmail.com>
2017-07-05 14:59:46 +0900

    add semicolon

--- src/es6/emojidexPalette/components/tabs/user_tabs/followers_tab.js
+++ src/es6/emojidexPalette/components/tabs/user_tabs/followers_tab.js
@@ -10,7 +10,7 @@ class FollowersTab {
       <div id='follow-followers' class='tab-pane'>
         <div class='users'></div>
       </div>
-    `)
+    `);
   }
 
   init() {
@@ -22,13 +22,13 @@ class FollowersTab {
         this.setUserButton(user_name);
         this.setUserInfo(user_name);
       }
-    })
+    });
   }
 
   setUserButton(user_name) {
     let user_button = $(`<div class='btn btn-default'>${user_name}</div>`).click(e => {
-      $(this.selector_tab_pane).find(`#${$(e.currentTarget).text()}`).addClass('on')
-    })
+      $(this.selector_tab_pane).find(`#${$(e.currentTarget).text()}`).addClass('on');
+    });
     $(this.selector_users).append(user_button);
   }
 
@@ -44,27 +44,27 @@ class FollowersTab {
       </div>
     `).click(e => {
       e.stopPropagation();
-    })
+    });
     user_info.find('.btn-close').click(() => {
       $(this.selector_tab_pane).find('*').removeClass('on');
     });
     $(this.selector_tab_pane).append(user_info);
 
-    this.setUserEmojisInfo(user_info)
+    this.setUserEmojisInfo(user_info);
   }
 
   setUserEmojisInfo(user_info, option = {}) {
-    const user_name = user_info.attr('id')
+    const user_name = user_info.attr('id');
     return this.EC.Indexes.user(user_name, undefined, option).then((response) => {
       user_info.data(response.meta);
       user_info.data({user_name: user_name, max_page: Math.ceil(response.meta.total_count / this.EC.limit ? response.meta.total_count / this.EC.limit : 1)});
 
-      user_info.find('.user-emoji-list').children().remove()
-      user_info.find('.followers-pagination').remove()
+      user_info.find('.user-emoji-list').children().remove();
+      user_info.find('.followers-pagination').remove();
 
-      user_info.find('.user-emoji-list').append(this.palette.setEmojiList('followers', response.emoji))
-      this.setPagination(user_info)
-    })
+      user_info.find('.user-emoji-list').append(this.palette.setEmojiList('followers', response.emoji));
+      this.setPagination(user_info);
+    });
   }
 
   setPagination(user_info) {
@@ -85,6 +85,6 @@ class FollowersTab {
         this.setUserEmojisInfo(user_info, option);
       }
     }
-    user_info.append(this.palette.getPagination('followers', prev_func, next_func, meta.page, meta.max_page))
+    user_info.append(this.palette.getPagination('followers', prev_func, next_func, meta.page, meta.max_page));
   }
 }
