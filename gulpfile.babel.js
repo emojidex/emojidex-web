92fdcaba9745017dbe34e6346734edc48b1c0eeb
snowsunny <snowsunny1107@gmail.com>
2017-07-14 13:22:26 +0900

    fix spec order

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -273,7 +273,10 @@ gulp.task('jasmine', () => {
     'dist/img/logo.png',
     'dist/css/document.min.css',
     'dist/css/emojidex.min.css',
-    'spec/**/*.js'
+    'spec/palette/user_login.js',
+    'spec/palette/*.js',
+    'spec/emojidexAutocomplete.js',
+    'spec/emojidexReplace.js'
   ];
   return gulp.src(testFiles)
     .pipe(watch(testFiles))
