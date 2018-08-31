fe22a47746e67a6187e30a51f83da44bb79a5036
Rika Yoshida <nazuki@phantom.industries>
2018-08-31 14:21:33 +0900

    wip

---
+++ spec/palette/customization.js
@@ -0,0 +1,82 @@
+describe('emojidexPalette:Customization', () => {
+  beforeAll(done => {
+    clearStorage().then(() => {
+      helperBefore();
+      preparePaletteButtons(done);
+    });
+  });
+
+  afterAll(() => {
+    closePalette();
+    helperAfter();
+  });
+
+  it('changes to a customization tab', done => {
+    $('#tab-content-customization').watch({
+      id: 'content_customization',
+      properties: 'prop_innerHTML',
+      watchChildren: true,
+      callback(data, i) {
+        if (data.vals[0].match(/customization-emoji-list/)) {
+          expect($('#tab-customization')).toHaveClass('active');
+          expect($('#tab-content-customization')).toHaveClass('active');
+          expect($('#tab-content-customization').find('img').length).toBeTruthy();
+          removeWatch($('#tab-content-customization'), 'content_customization');
+          done();
+        }
+      }
+    });
+
+    showPalette(() => {
+      $('#tab-customization a').click();
+    });
+  });
+
+  it('switches to the next page', done => {
+    $('#tab-content-customization').watch({
+      id: 'content_customization',
+      properties: 'prop_innerHTML',
+      watchChildren: true,
+      callback(data, i, mutations) {
+        expect($(data.vals[0]).find('ul.pagination li.disabled span').text().substr(0, 1)).toBe('2');
+        removeWatch($('#tab-content-customization'), 'content_customization');
+        done();
+      }
+    });
+    $('#tab-content-customization').find('.pagination .palette-pager')[1].click();
+  });
+
+  it('switches to the previous page', done => {
+    $('#tab-content-customization').watch({
+      id: 'content_customization',
+      properties: 'prop_innerHTML',
+      watchChildren: true,
+      callback(data, i, mutations) {
+        expect($(data.vals[0]).find('ul.pagination li.palette-num span').text().substr(0, 1)).toBe('1');
+        removeWatch($('#tab-content-customization'), 'content_customization');
+        done();
+      }
+    });
+    $('#tab-content-customization').find('.pagination .palette-pager')[0].click();
+  });
+
+  it('open the customization content', done => {
+    $('#tab-content-customization').watch({
+      id: 'content_customization',
+      properties: 'prop_innerHTML',
+      watchChildren: true,
+      callback(data, i, mutations) {
+        if (data.vals[0].match(/customization-info/)) {
+          expect($(data.vals[0]).find('.customization-preview').length).toBeTruthy();
+          expect($(data.vals[0]).find('.customization-select').length).toBeTruthy();
+          removeWatch($('#tab-content-customization'), 'content_customization');
+          done();
+        }
+      }
+    });
+
+    specTimer(1000).then(() => {
+      $($('.customization-emoji-list .emoji-btn')[0]).click();
+    });
+  });
+});
