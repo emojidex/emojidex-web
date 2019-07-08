1f4248288a329ab19f99dcf32fa07c9a0c852091
Rika Yoshida <nazuki@phantom.industries>
2019-07-08 14:33:21 +0900

    [wip] fix index.js

--- src/es6/index.js
+++ src/es6/index.js
@@ -6,13 +6,15 @@ import 'bootstrap-sass/assets/javascripts/bootstrap/tab.js'
 import 'jquery.caret/dist/jquery.caret.min.js'
 import './../vendor/jquery-ui-1.12.1.custom/jquery-ui.min.js'
 
-const plugins = [EmojidexAutocomplete, EmojidexPalette, EmojidexReplace];
+/* eslint-disable no-undef */
+const plugins = [EmojidexAutocomplete, EmojidexPalette, EmojidexReplace]
 for (const Plugin of plugins) {
-  $.fn[Plugin.getName()] = function(options) {
-    return this.each(function() {
+  $.fn[Plugin.getName()] = function (options) {
+    return this.each(function () {
       if (!$.data(this, `plugin_${Plugin.getName()}`)) {
         return $.data(this, `plugin_${Plugin.getName()}`, new Plugin(this, options))
       }
     })
   }
 }
+/* eslint-enable no-undef */
