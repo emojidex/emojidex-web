717bf95a76b4e576507f875ba29bec8e5e202def
Rika Yoshida <nazuki@phantom.industries>
2019-07-08 16:02:38 +0900

    [wip] change filename, fix emojidexPalette/*.js

---
+++ src/es6/emojidexPalette/components/tabs/userTabs/followers.js
@@ -0,0 +1,94 @@
+/* eslint-disable no-undef */
+export default class FollowersTab {
+  constructor(userTab) {
+    this.EC = userTab.palette.EC
+    this.palette = userTab.palette
+
+    this.selectorTabPane = '#emojidex-emoji-palette #follow-followers'
+    this.selectorUsers = `${this.selectorTabPane} > .users`
+
+    this.tabPane = $(`
+      <div id='follow-followers' class='tab-pane'>
+        <div class='users'></div>
+      </div>
+    `)
+  }
+
+  init() {
+    $(this.selectorUsers).children().remove()
+    $(`${this.selectorTabPane} > .user-info`).remove()
+
+    this.EC.User.Follow.getFollowers(followers => {
+      for (const userName of followers) {
+        this.setUserButton(userName)
+        this.setUserInfo(userName)
+      }
+    })
+  }
+
+  setUserButton(userName) {
+    const userButton = $(`<div class='btn btn-default'>${userName}</div>`).click(e => {
+      $(this.selectorTabPane).find(`#${$(e.currentTarget).text()}`).addClass('on')
+    })
+    $(this.selectorUsers).append(userButton)
+  }
+
+  setUserInfo(userName) {
+    const userInfo = $(`
+      <div id='${userName}' class='user-info'>
+        <div class='btn-close' aria-hidden='true'><i class='emjdx-abstract flip-vertical'></i></div>
+        <div class='user-name'>${userName}</div>
+        <div class="clearfix"/>
+        <hr>
+        <div class="user-emoji-list clearfix">
+        </div>
+      </div>
+    `).click(e => {
+      e.stopPropagation()
+    })
+    userInfo.find('.btn-close').click(() => {
+      $(this.selectorTabPane).find('*').removeClass('on')
+    })
+    $(this.selectorTabPane).append(userInfo)
+
+    this.setUserEmojisInfo(userInfo)
+  }
+
+  setUserEmojisInfo(userInfo, option = {}) {
+    const userName = userInfo.attr('id')
+    return this.EC.Indexes.user(userName, undefined, option).then(response => {
+      userInfo.data(response.meta)
+      userInfo.data({ userName, maxPage: Math.ceil(response.meta.total_count / this.EC.limit ? response.meta.total_count / this.EC.limit : 1) })
+
+      userInfo.find('.user-emoji-list').children().remove()
+      userInfo.find('.followers-pagination').remove()
+
+      userInfo.find('.user-emoji-list').append(this.palette.setEmojiList('followers', response.emoji))
+      this.setPagination(userInfo)
+    })
+  }
+
+  setPagination(userInfo) {
+    const meta = userInfo.data()
+    if (!this.EC.User.authInfo.premium) {
+      meta.maxPage = 1
+    }
+
+    const prevFunc = () => {
+      const option = { page: meta.page - 1 }
+      if (option.page > 0) {
+        this.setUserEmojisInfo(userInfo, option)
+      }
+    }
+
+    const nextFunc = () => {
+      const option = { page: meta.page + 1 }
+      if (option.page <= meta.maxPage) {
+        this.setUserEmojisInfo(userInfo, option)
+      }
+    }
+
+    userInfo.append(this.palette.getPagination('followers', prevFunc, nextFunc, meta.page, meta.maxPage))
+  }
+}
+/* eslint-enable no-undef */
---
+++ src/es6/emojidexPalette/components/tabs/userTabs/history.js
@@ -0,0 +1,38 @@
+export default class HistoryTab {
+  constructor(userTab) {
+    this.EC = userTab.palette.EC
+    this.palette = userTab.palette
+    this.tabPane = $('<div class=\'tab-pane\' id=\'tab-content-user-history\'></div>') // eslint-disable-line no-undef
+  }
+
+  createTabContent() {
+    return this.EC.User.History.get().then(response => {
+      return this.setHistoryEmoji(response)
+    })
+  }
+
+  setHistoryEmoji(history) {
+    this.tabPane.children().remove()
+    this.tabPane.append(this.palette.setEmojiList('history', history))
+    return this.createPagination()
+  }
+
+  createPagination() {
+    const { meta } = this.EC.User.History
+    const curPage = meta.total_count === 0 ? 0 : meta.page
+    let maxPage = curPage === 0 ? 0 : Math.ceil(meta.total_count / this.EC.limit)
+    if (!this.EC.User.authInfo.premium) {
+      maxPage = 1
+    }
+
+    const callback = response => {
+      this.tabPane.children().remove()
+      this.tabPane.append(this.setHistoryEmoji(response))
+    }
+
+    const prevFunc = () => this.EC.User.History.prev(callback)
+    const nextFunc = () => this.EC.User.History.next(callback)
+
+    return this.tabPane.append(this.palette.getPagination('history', prevFunc, nextFunc, curPage, maxPage))
+  }
+}
